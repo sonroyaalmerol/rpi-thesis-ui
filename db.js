1f47299ff17d26e1ac56fd07b7f82d412dc9119b
Son Roy Almerol <saalmerol@pm.me>
2019-08-16 18:32:47 +0800

    add dl to csv

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const PouchDB = require("pouchdb")
 PouchDB.plugin(require('pouchdb-find'))
 
-const db = new PouchDB('http://127.0.0.1:5984/thesis-data-demo')
+const db = new PouchDB('http://127.0.0.1:5984/thesis-data')
 exports.db = db
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('views/vendors'))
 const port = 3000
+const Papa = require('papaparse')
+const moment = require('moment')
 var db = require('./db').db
 
 
@@ -16,6 +18,22 @@ app.get('/', async (req, res) => {
   res.render('table', { 
     data: result.docs
   })
+})
+app.get('/csv', async (req, res) => {
+  await db.createIndex({
+    index: { fields: [{'timestamp': 'desc'}] }
+  })
+  let result = await db.find({ selector: { timestamp: {$exists: true} }, sort: [{timestamp: 'desc'}] })
+  console.log(result.docs)
+  result.docs.map((res) => {
+      delete res._id
+      delete res._rev
+      res.timestamp = moment(res.timestamp).format("MMM DD YYYY, h:mm:ss a")
+  })
+  let csv = Papa.unparse(result.docs)
+  res.setHeader('Content-Type', 'text/csv')
+  res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'thesis-data-' + Date.now() + '.csv\"')
+  res.send(csv)
 })
 
 app.listen(port, '127.0.0.1', () => console.log(`Thesis API listening on port ${port}!`))
