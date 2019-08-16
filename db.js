eb733be8df32f461949bad0ab927ba22bd825d40
Son Roy Almerol <saalmerol@pm.me>
2019-08-16 17:48:03 +0800

    first commit

---
+++ db.js
@@ -0,0 +1,5 @@
+const PouchDB = require("pouchdb")
+PouchDB.plugin(require('pouchdb-find'))
+
+const db = new PouchDB('http://127.0.0.1:5984/thesis-data-demo')
+exports.db = db
---
+++ index.js
@@ -0,0 +1,21 @@
+// required libraries
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('views/vendors'))
+const port = 3000
+var db = require('./db').db
+
+
+app.get('/', async (req, res) => {
+  await db.createIndex({
+    index: { fields: [{'timestamp': 'desc'}] }
+  })
+  let result = await db.find({ selector: { timestamp: {$exists: true} }, sort: [{timestamp: 'desc'}] })
+  console.log(result.docs)
+  res.render('table', { 
+    data: result.docs
+  })
+})
+
+app.listen(port, () => console.log(`Thesis API listening on port ${port}!`))
